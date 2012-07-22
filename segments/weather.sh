9962e51d438ce345af03e25adba6c7e08a7c733a
Yoshiteru Negishi <negishi@cookpad.com>
2012-07-22 19:08:34 +0900

    changed location

--- segments/weather.sh
+++ segments/weather.sh
@@ -2,7 +2,7 @@
 # Prints the current weather in Celsius, Fahrenheits or lord Kelvins. The forecast is cached and updated with a period of $update_period.
 
 # You location. Find a string that works for you by Googling on "weather in <location-string>"
-location="Lund, Sweden"
+location="Tokyo, Japan"
 
 # Can be any of {c,f,k}.
 unit="c"
